8e9d8e4f4a566ec0c24bb6bca66d86c739aa9a69
VascoRegal <vascoregal24@gmail.com>
2021-12-07 14:37:20 +0000

    rest api initializer

---
+++ BackEnd/api/ShopAholytics/src/test/java/ies/g52/ShopAholytics/ShopAholyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.g52.ShopAholytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAholyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
