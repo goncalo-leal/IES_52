13911aaf8605fbad5a1d8d71dfb29157c8697592
VascoRegal <vascoregal24@gmail.com>
2021-12-09 17:33:32 +0000

    classes mq para  a api (nao testado)

---
+++ api/ShopAholytics/src/main/java/ies/g52/ShopAholytics/rabbitmq/MQConsumer.java
@@ -0,0 +1,14 @@
+package ies.g52.ShopAholytics.rabbitmq;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MQConsumer {
+    @RabbitListener(queues = MQConfig.QUEUE)
+    public void consumer(Message message) {
+        System.out.println(message);
+    }
+}
+
