182dec1126ab6d34493ddb08c8b233802fb25e20
alexandre <alexandreserras@ua.pt>
2021-12-16 15:11:06 +0000

    endpoint para os parks e stores de um shopping , deve ser isto que o devops quer

--- api/ShopAholytics/src/main/java/ies/g52/ShopAholytics/controller/ShoppingController.java
+++ api/ShopAholytics/src/main/java/ies/g52/ShopAholytics/controller/ShoppingController.java
@@ -1,10 +1,14 @@
 package ies.g52.ShopAholytics.controller;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import ies.g52.ShopAholytics.models.Park;
 import ies.g52.ShopAholytics.models.Shopping;
+import ies.g52.ShopAholytics.models.Store;
 import ies.g52.ShopAholytics.services.ShoppingServices;
 
 
@@ -23,6 +27,25 @@ public class ShoppingController {
         return null;
     }
 
+    @GetMapping("/storesShopping/{pid}")
+    public Set<Store> findAllStoresShopping(@PathVariable int pid) {
+        Shopping s = shoppingService.getShoppingById(pid);
+
+        Set<Store> ret=s.getStores();
+        
+        return ret;
+    }
+
+    @GetMapping("/parksShopping/{pid}")
+    public Set<Park> findAllParksShopping(@PathVariable int pid) {
+        Shopping s = shoppingService.getShoppingById(pid);
+
+        Set<Park> ret=s.getParks();
+        
+        return ret;
+    }
+
+    
     @GetMapping("/Shoppings")
     public List<Shopping> findAllShopping() {
         List<Shopping> a = shoppingService.getShopping();
