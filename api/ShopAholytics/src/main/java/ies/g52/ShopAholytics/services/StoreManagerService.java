07c30d4c5645a509b0c42c3ff92202413f6037c1
VascoRegal <vascoregal24@gmail.com>
2021-12-16 19:27:14 +0000

    frontend login, add store manager, show stores of shopping and search

--- api/ShopAholytics/src/main/java/ies/g52/ShopAholytics/services/StoreManagerService.java
+++ api/ShopAholytics/src/main/java/ies/g52/ShopAholytics/services/StoreManagerService.java
@@ -1,10 +1,14 @@
 package ies.g52.ShopAholytics.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ies.g52.ShopAholytics.models.Shopping;
+import ies.g52.ShopAholytics.models.Store;
 import ies.g52.ShopAholytics.models.StoreManager;
 import ies.g52.ShopAholytics.repository.StoreManagerRepository;
 
@@ -13,6 +17,9 @@ import ies.g52.ShopAholytics.repository.StoreManagerRepository;
 public class StoreManagerService {
     @Autowired
     private StoreManagerRepository repository;
+
+    @Autowired
+    private ShoppingServices shoppingServices;
 
     public StoreManager saveStoreManager(StoreManager StoreManager) {
         return repository.save(StoreManager);
@@ -38,6 +45,23 @@ public class StoreManagerService {
     public StoreManager updateStoreManager(StoreManager StoreManager) {
         StoreManager existingStoreManager = repository.findById((int)StoreManager.getId()).orElse(null);
         return repository.save(existingStoreManager);
+    }
+
+    public List<StoreManager> storeManagersOfShopping(int id) {
+
+        Shopping s = shoppingServices.getShoppingById(id);
+            
+        Set<Store> stores=s.getStores();
+        List<StoreManager> managers = this.getStoreManagers();
+        List<StoreManager> ret= new ArrayList<>();
+
+        for (StoreManager a: managers){
+            if (stores.contains(a.getStore()) ){
+                ret.add(a);
+            }
+        }
+        
+        return ret;
     }
 
    
