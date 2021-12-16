ac64b3d41ea6a4cc351a14f5ab36c385a52257f4
VascoRegal <vascoregal24@gmail.com>
2021-12-16 19:28:43 +0000

    frontend login, add store manager, show stores of shopping and search

--- FrontEnd/js/common.js
+++ FrontEnd/js/common.js
@@ -1,8 +1,16 @@
 import SessionManager from "./session.js";
 
+
 const updateView = function () {
+    if (SessionManager.get("session") === null) {
+        alert("Please log in!");
+        window.location.href = "./login.html";
+    }
+
     $("#loginBttn").text("Log Out");
     $("#sm_name").text(SessionManager.get("session").user.name);
+
+    $("body").fadeIn(300);
 }
 
 export default updateView;
