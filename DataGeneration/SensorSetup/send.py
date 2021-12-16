f2da95a1145ea9dac0e86a21e228334d8f12b3d6
goncalo-leal <goncalolealsilva@ua.pt>
2021-12-16 16:02:44 +0000

    interface (incompleta) e troca de mensagens a funcionar

--- DataGeneration/SensorSetup/send.py
+++ DataGeneration/SensorSetup/send.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 import pika
+import json
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host='localhost', port='5672'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='QUEUE', durable=True)
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+json.dumps(x)
+
+channel.basic_publish(exchange='EN', routing_key='', body=json.dumps(x))
+print(" [x] Sent '", json.dumps(x),"'")
 connection.close()
