3acfd5f5cf57bfad85af8d59d380e66d75d1e472
bernas04 <joaobernardo0@ua.pt>
2021-12-27 12:48:06 +0000

    userstates in frontend

--- FrontEnd/js/manage.js
+++ FrontEnd/js/manage.js
@@ -35,16 +35,45 @@ const loadTable = function() {
     })
 }
 
+const loadUserStates = function() {
+    var estados='';
+    var selectVal='';
+    var selectInput='';
 
-const renderTable = function (data) {
-    $("#managers_body").empty();
-    data.forEach(function(e, i) {
-        $("#managers_body").append(trTemplate(e.user.name, e.user.email, e.store.name, true));
+    $.ajax({
+        url: consts.BASE_URL + '/api/UserStates/',
+        type: "GET", 
+        contentType: "application/json",
+        dataType: "json",
+        success: function(data) {
+            if (data) {
+                estados = data;
+                for (var i=0; i< estados.length; i++){
+                    selectVal = estados[i].description;
+                    selectInput  = '<option value='+ selectVal +'>'+ selectVal +'</option>';
+                    $('select[name="states"]').append(selectInput);
+                }
+            } else {
+                console.log("No store managers for this shopping");
+            }
+        },
+        error: function() {
+            console.log("erro na call");
+        }
     })
 }
 
 
-const trTemplate = function (name, email, shop, active) {
+const renderTable = function (data) {
+    $("#managers_body").empty();
+    data.forEach(function(e, i) {
+        $("#managers_body").append(trTemplate(e.user.name, e.user.email, e.store.name, e.user.state.description));
+    })
+    loadUserStates();
+}
+
+
+const trTemplate = function (name, email, shop, description) {
     return `
     <tr>
         <td>
@@ -56,8 +85,10 @@ const trTemplate = function (name, email, shop, active) {
         <td class="text-center">
             ${shop}
         </td>
-        <td class="text-right">
-            <input type="checkbox" name="my-checkbox" checked data-bootstrap-switch data-off-color="danger">
+        <td class="text-center">
+            <select name="states" class="browser-default custom-select" style="max-width:200px;">
+                <option selected="${description}">${description}</option>
+            </select>
         </td>
     </tr>
     `
