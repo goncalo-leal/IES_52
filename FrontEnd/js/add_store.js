4a207bbdd7cf9fa05546786841cca64aed72d22e
goncalo-leal <goncalolealsilva@ua.pt>
2022-01-12 15:03:57 +0000

    add store

--- FrontEnd/js/add_store.js
+++ FrontEnd/js/add_store.js
@@ -3,7 +3,18 @@ import SessionManager from "./session.js";
 import updateView from "./common.js"
 
 $(document).ready(function() {
+    $('#opening_timepicker').datetimepicker({
+        format: 'HH:mm',
+        use24hours: true,
+    });
+    $('#closing_timepicker').datetimepicker({
+        format: 'HH:mm',
+        use24hours: true,
+    });
+
+    $("#capacity").on('input', function(e) {
+        $(this).val($(this).val().replace(/[^0-9]/g, ''));
+    });
 
     updateView();
-
 });
