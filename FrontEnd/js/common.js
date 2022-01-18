653891cbfcc513e1dd02551064e43bdcd7c9d1b9
VascoRegal <vascoregal24@gmail.com>
2022-01-18 00:00:03 +0000

    fix infinite redirect no login

--- FrontEnd/js/common.js
+++ FrontEnd/js/common.js
@@ -2,7 +2,7 @@ import SessionManager from "./session.js";
 
 
 const updateView = function () {
-    if (SessionManager.get("session") === null) {
+    if (SessionManager.get("session") === null && window.location.pathname != "/index.html") {
         window.location.href = "./index.html";
     }
 
