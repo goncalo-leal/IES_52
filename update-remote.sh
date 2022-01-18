c744974fd96db0fd3f929fb7a891d7f95f5878bf
VascoRegal <vascoregal24@gmail.com>
2022-01-18 14:59:23 +0000

    script

--- update-remote.sh
+++ update-remote.sh
@@ -3,8 +3,8 @@
 jar_local=./api/ShopAholytics/target/ShopAholytics-0.0.1-SNAPSHOT.jar
 frontend_local=./FrontEnd
 
-jar_remote=
-frontend_remote=
+jar_remote=~/ies_52/build/api/api.jar
+frontend_remote=~/ies_52/src/client
 
 remote=deti-engsoft-15
 
@@ -15,7 +15,7 @@ read user
 printf "[+] Copy jar..."
 scp ${jar_local} ${user}@${remote}:${jar_remote}
 printf "[+] Copy frontend..."
-scp ${frontend_local}*.html ${frontend_local}/js ${user}@${remote}:${frontend_remote}
+scp ${frontend_local}/*.html ${frontend_local}/js ${user}@${remote}:${frontend_remote}
 printf "[+] run.sh" 
 ssh ${user}@${remote} "cd ies_52; sudo ./run.sh"
 
